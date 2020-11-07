1280999898f0e4f55fe4ca74e55b104792b508ce
samyLaurent <bakonsamuelguylaurent@gmail.com>
2020-11-07 19:47:27 +0100

    Added setup class for configs

--- Src/Setup/Setup.php
+++ Src/Setup/Setup.php
@@ -8,10 +8,12 @@
 
 namespace MyCoolPay\Setup;
 
+use Composer\Script\Event;
+use Composer\Installer\PackageEvent;
 
 class Setup
 {
-    public static function install()
+    public static function install(Event $event)
     {
         mkdir(__DIR__.'/setup.yaml');
     }
